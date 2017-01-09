aa618212b3a13dc7731cbc8e0d6b970b819ab703
leowolf749 <leowolf749@gmail.com>
2017-01-08 21:51:02 -0500

    Added a textbox to the menu to search for foods. Made the add food view 'display none'.

--- public/app.js
+++ public/app.js
@@ -13,6 +13,16 @@ let food = [
         description: "1/2lb beef patty w/ cheddar cheese, lettuce, tomato, and onion on a sesame seed bun",
         price: "$8.59",
     },
+    {
+        foodName: "Smoked Wings",
+        description: "10 smoked chicken wings tossed in your choice of wing sauce",
+        price: "$9.59",
+    },
+    {
+        foodName: "BLT",
+        description: "Bacon, lettuce, tomato, mayonaise on your choice of bread",
+        price: "$5.59",
+    }
 ];
 
 function showFood(food) {
@@ -29,6 +39,7 @@ function showFood(food) {
     parent.appendChild(child);
 }
 
+/* this doesn't work yet */
 function addFood(food) {
     let input = document.querySelector('#add-food-item');
     
